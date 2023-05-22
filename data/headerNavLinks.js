8797b46d80fac50ec181ad3add0b4f2d5802a11d
William Qian <william.z.qian@gmail.com>
2023-05-22 23:35:08 +0800

    Update on Hello

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -3,6 +3,7 @@ const headerNavLinks = [
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
+  { href: 'https://hi.williamq.me', title: 'Hello' },
 ]
 
 export default headerNavLinks
